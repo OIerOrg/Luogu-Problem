15f498516128c7e1a7faf593d470d3f76e178955
OIer Kimi <15381388023@163.com>
2024-10-06 20:46:04 +0800

    fix: 修复 Katex

--- main.py
+++ main.py
@@ -63,6 +63,9 @@ diff: {difficulty}
 pid: {pid}
 tag: {problem['tags']}
 ---
+
+<script src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML" type="text/javascript"></script>
+
 # {title}
 """
 
